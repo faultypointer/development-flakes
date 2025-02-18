74d723158231af292cffe4d0c881a9b62d346919
faultypointer <121874711+faultypointer@users.noreply.github.com>
2025-02-18 22:18:40 +0545

    Create flake.nix

---
+++ zig-raylib/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix-flake-based Zig development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            zig
+            zls
+            lldb
+            raylib
+            raygui
+            pkgs.xorg.libX11
+            pkgs.xorg.libX11.dev
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXi
+            pkgs.xorg.libXinerama
+            pkgs.xorg.libXrandr
+            libGL
+            glfw
+            wayland
+            glibc
+          ];
+        };
+      });
+    };
+}
